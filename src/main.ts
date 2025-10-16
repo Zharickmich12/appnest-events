a637ae074312aa7c4d15e0af549edb8b8ae6d5c6
EileenMendoza18 <eileenjml4816@gmail.com>
2025-10-15 22:14:48 -0500

    Configuracion de TypeORM y base de datos enlazada

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true,
+    forbidNonWhitelisted: true, // lanza error si llega un campo inesperado
+      transform: true, // transforma automáticamente tipos al DTO
+  }))
+
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
+
+  console.log(`Application running on: http://localhost:${port}`)
 }
 bootstrap();
